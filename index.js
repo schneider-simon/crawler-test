1f904344b1c1934251cacdc6e9e6abacaa66c85a
Simon Schneider <s.schneider@zwei14.de>
2017-01-12 23:05:52 +0100

    Multiple steps for example page.

--- index.js
+++ index.js
@@ -12,37 +12,29 @@ const STEP_TABLE_OF_CONTENTS = 'STEP_TABLE_OF_CONTENTS'
 const STEP_TOPIC = 'STEP_TOPIC'
 const STEP_VIDEO = 'STEP_VIDEO'
 
-const pageCrawler = new PageCrawler(({response, $, step, data, queue, finish}) => {
-    switch (step) {
-        case STEP_INITIAL:
-            $('a[href$="/inhaltsverzeichnis/"]').forEach((element) => {
-                const href = element.href;
-                data.tableOfContents = href;
-                queue(href, STEP_TABLE_OF_CONTENTS, data);
-            })
-            break;
-        case STEP_TABLE_OF_CONTENTS:
-            $('.pagination:first-of-type a[href*="?page="]').forEach((element) => {
-                const href = element.href;
-                data.tableOfContents = href;
-                queue(href, STEP_TABLE_OF_CONTENTS, data);
-            })
-            break;
-        case STEP_TOPIC:
-            break;
-        case STEP_VIDEO:
-            break;
-        default:
-            console.error(`Unknown step ${ step }.`);
-    }
-});
 
-pageCrawler.run('https://oberprima.com/nachhilfevideos/');
-
-const PageCrawler = (handle) => {
+function PageCrawler(handle) {
     this.handle = handle;
+    this.history = [];
 
-    this.queue = (uri, step = STEP_INITIAL, data = {}) => {
+    this.queue = (uri, step = STEP_INITIAL, _data = {}) => {
+        const data = cloneDeep(_data);
+        data.uri = uri;
+
+        if(this.history.indexOf(uri) !== -1){
+            return;
+        }
+
+        this.history.push(uri);
+
+        if(!data.crawlPath){
+            data.crawlPath = [];
+        }
+
+        data.crawlPath.push(uri);
+
+        console.log('queue', uri);
+
         return c.queue([{
             uri: uri,
             callback: (error, response, done) => {
@@ -56,7 +48,7 @@ const PageCrawler = (handle) => {
                     response,
                     $: response.$,
                     step,
-                    data: cloneDeep(data),
+                    data,
                     queue: this.queue,
                     finish: this.finish
                 });
@@ -74,3 +66,31 @@ const PageCrawler = (handle) => {
         return this.queue(uri, STEP_INITIAL, {});
     }
 }
+
+
+const pageCrawler = new PageCrawler(({response, $, step, data, queue, finish}) => {
+    switch (step) {
+        case STEP_INITIAL:
+            $('a[href$="/inhaltsverzeichnis/"]').each((key, element) => {
+                const href = url.resolve(data.uri, $(element).attr('href'));
+                data.tableOfContents = href;
+                queue(href, STEP_TABLE_OF_CONTENTS, data);
+            })
+            break;
+        case STEP_TABLE_OF_CONTENTS:
+            $('.pagination:first-of-type a[href*="?page="]').each((key, element) => {
+                const href = url.resolve(data.uri, $(element).attr('href'));
+                data.tableOfContents = href;
+                queue(href, STEP_TABLE_OF_CONTENTS, data);
+            })
+            break;
+        case STEP_TOPIC:
+            break;
+        case STEP_VIDEO:
+            break;
+        default:
+            console.error(`Unknown step ${ step }.`);
+    }
+});
+
+pageCrawler.run('https://oberprima.com/nachhilfevideos/');
